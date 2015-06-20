1466c1ad1001f97b2de7a6d85eae4854f0fb3b13
JeppeHallgren <jeppe@hallgren.com>
2015-06-21 00:05:16 +0100

    setup sample cli

---
+++ src/ai/nxt/seqpred/SequencePredictorCli.java
@@ -0,0 +1,10 @@
+package ai.nxt.seqpred;
+
+/**
+ * Created by Jeppe Hallgren on 21/06/15.
+ */
+public class SequencePredictorCli {
+    public static void main(String args[]) {
+        System.out.println("Hello world");
+    }
+}
