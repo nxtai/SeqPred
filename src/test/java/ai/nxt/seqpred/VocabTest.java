eb8596a3a3f62918d22c09a5124d01869374aa2c
JeppeHallgren <jeppe@hallgren.com>
2015-06-28 20:12:03 +0200

    add test case for vocab

---
+++ src/test/java/ai/nxt/seqpred/VocabTest.java
@@ -0,0 +1,22 @@
+package ai.nxt.seqpred;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+/**
+ * Created by Jeppe Hallgren on 28/06/15.
+ */
+public class VocabTest extends TestCase {
+
+    @Test
+    public void testGetVocabSize() throws Exception {
+        Vocab vocab = new Vocab("src/test/resources/data/vocab-test-set.txt");
+        assertEquals(vocab.getVocabSize(),12);
+    }
+
+    @Test
+    public void testGetTrainingFileSize() throws Exception {
+        Vocab vocab = new Vocab("src/test/resources/data/vocab-test-set.txt");
+        assertEquals(vocab.getTrainingFileSize(),16);
+    }
+}
